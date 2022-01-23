6e22770e06621730f9fd1a4a2b29c53b059ddfb4
dartagnan sams <dartagnansams1@icloud.com>
2022-01-23 11:20:19 -0500

    changed socket url to rediss and changed google oauth client id

--- EaglePlannerProject/settings.py
+++ EaglePlannerProject/settings.py
@@ -97,7 +97,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get("REDIS_URL")],
+            "hosts": [os.environ.get("REDIS_TLS_URL")],
         },
     },
 }
