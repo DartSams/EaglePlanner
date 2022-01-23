9069d513fe5868156febbc1f20205436f7b66db0
dartagnan sams <dartagnansams1@icloud.com>
2022-01-22 20:01:27 -0500

    added static_root in settings.py

--- EaglePlannerProject/settings.py
+++ EaglePlannerProject/settings.py
@@ -136,3 +136,7 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staic')
+# Activate Django-Heroku.
+# django_heroku.settings(locals())
