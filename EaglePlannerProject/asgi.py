989029a5cfd5049cb402d23c456808cc40291c75
dartagnan sams <dartagnansams1@icloud.com>
2022-01-23 08:43:28 -0500

    changed asgi layout and changed procfile worker

--- EaglePlannerProject/asgi.py
+++ EaglePlannerProject/asgi.py
@@ -7,10 +7,32 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EaglePlannerProject.settings')
+
+# application = get_asgi_application()
+
+
 import os
+import channels
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "therapist_portal.settings")
+django.setup()
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import EaglePlannerProject.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EaglePlannerProject.settings')
-
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            EaglePlannerProject.routing.websocket_urlpatterns
+        )
+    ),
+})
--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: daphne -b 0.0.0.0 -p $PORT EaglePlannerProject.asgi:application
+release: python manage.py migrate
+web: daphne EaglePlannerProject.asgi:application --port $PORT --bind 0.0.0.0
+worker: python manage.py runworker channel_layer
